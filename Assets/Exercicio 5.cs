468d94d13b180ff49cff049dae5c5fe127d94a99
ThiagoMonteiro12 <thiagoben3@gmail.com>
2024-11-05 14:46:38 -0300

    Exc 7

--- Assets/Exercicio 5.cs
+++ Assets/Exercicio 5.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//5. (Pontuação de missão) Após completar uma missão, o jogador
+//recebe pontos. Verifique se ele ganhou mais de 50 pontos. Se sim,
+//exiba "Missão bem-sucedida"; caso contrário, "Missão
+//incompleta".
 public class Exercicio5 : MonoBehaviour
 {
+
+    [SerializeField] int pontos;
+    [SerializeField] System.Random xp = new System.Random();
+
     // Start is called before the first frame update
     void Start()
     {
+            int pontos = xp.Next(1, 120);
+
+        if (pontos > 50) {
+
+            print("Passooooooou");
+        }
+        else
+        {
+            print("Não passoooooooooooou");
+        }
         
     }
 
